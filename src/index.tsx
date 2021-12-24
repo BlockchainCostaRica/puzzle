e96efdba9fc9317c03ccbee132928d5ec3c97461
lzhabo <lidia.zhabo99@gmail.com>
2021-12-23 19:07:07 -0500

    added error store

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as reactAlert from "react-alert";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "normalize.css";
 import { RootStore, storesContext } from "@stores";
+import AlertTemplate from "@components/Header/AlertTemplate";
 
 const mobxStore = new RootStore();
-
+const options = {
+  timeout: 5000,
+  position: reactAlert.positions.TOP_RIGHT,
+};
 //todo add ReactGA support
 // const trackingId = "G-W203LN8Q6R";
 // ReactGA.initialize(trackingId);
@@ -20,7 +25,9 @@ ReactDOM.render(
   <React.StrictMode>
     <storesContext.Provider value={mobxStore}>
       <Router>
-        <App />
+        <reactAlert.Provider template={AlertTemplate} {...options}>
+          <App />
+        </reactAlert.Provider>
       </Router>
     </storesContext.Provider>
   </React.StrictMode>,
