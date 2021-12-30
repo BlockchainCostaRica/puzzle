54510b865efbb7a50f98dcebdda7aa0f72aad0f0
Олексiй <alexnagorny.an@gmail.com>
2021-12-30 05:41:25 +0300

    :recycle: fix safari table bug

--- src/screens/Landing/TokensList/ShowMoreButton.tsx
+++ src/screens/Landing/TokensList/ShowMoreButton.tsx
@@ -7,26 +7,21 @@ interface IProps {
 }
 
 const Root = styled.tr`
-  position: relative;
+  cursor: pointer;
 `;
 
 const TextButton = styled(Text)`
-  position: absolute;
   white-space: nowrap;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 16px;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
 `;
 
 const ShowMoreButton: React.FC<IProps> = ({ onClick, children }) => {
   return (
-    <Root onClick={onClick} style={{ position: "relative" }}>
-      <td>
+    <Root onClick={onClick}>
+      <td colSpan={3} style={{ width: "unset" }}>
         <TextButton weight={500} type="secondary">
           {children}
         </TextButton>
