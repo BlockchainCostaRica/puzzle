de98f9f83995ed1b8778a670d410f57dbb9537ec
Vladimir <izhur27@gmail.com>
2022-01-25 18:27:27 +0200

    Update InvestToPoolInterfaceVM.tsx

--- src/screens/InvestToPoolInterface/InvestToPoolInterfaceVM.tsx
+++ src/screens/InvestToPoolInterface/InvestToPoolInterfaceVM.tsx
@@ -229,7 +229,7 @@ class InvestToPoolInterfaceVM {
       return;
     }
     return this.rootStore.accountStore.invoke({
-      dApp: this.pool.layer2Address,
+      dApp: this.pool.address,
       payment: [],
       call: {
         function: "claimIndexRewards",
