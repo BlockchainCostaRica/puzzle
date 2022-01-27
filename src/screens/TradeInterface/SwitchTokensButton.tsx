d65dcb480926a146d2829a2704379aad2704b0db
Олексiй <alexnagorny.an@gmail.com>
2022-01-27 18:34:11 +0300

    :twisted_rightwards_arrows: vm.price format fixed

--- src/screens/TradeInterface/SwitchTokensButton.tsx
+++ src/screens/TradeInterface/SwitchTokensButton.tsx
@@ -47,7 +47,7 @@ const SwitchTokensButton: React.FC<IProps> = ({ ...rest }) => {
       <SizedBox width={8} />
       <Text>
         {!vm.synchronizing ? (
-          `1 ${vm.token0?.symbol} = ~ ${vm.price?.toFormat(8) ?? "—"} ${
+          `1 ${vm.token0?.symbol} = ~ ${vm.price?.toFormat(4) ?? "—"} ${
             vm.token1?.symbol
           }`
         ) : (
